c89cc5e86be917f7124088d64dbfe53e2ed8d089
developer-nome <williamhorn100@gmail.com>
2024-12-27 13:05:38 -0600

    Update main.py
    Streamlined code

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ from fastapi.staticfiles import StaticFiles
 from git import Repo
 from langchain_community.document_loaders.generic import GenericLoader
 from langchain_community.document_loaders.parsers import LanguageParser
-from langchain.callbacks import AsyncIteratorCallbackHandler
 from langchain.text_splitter import Language
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from openai import AsyncOpenAI
-from openai import OpenAI
 from pydantic import BaseModel
 
 import datetime
 import os
+import pandas as pd
 
 load_dotenv()
 
@@ -60,6 +59,10 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 async def favicon():
     return FileResponse(favicon_path)
 
+@app.get("/")
+def read_root():
+    model = os.getenv("LLM_MODEL")
+    return {model}
 
 @app.post("/items/")
 async def create_item(gitRepoToInspect: GitRepoToInspect):
@@ -88,145 +91,25 @@ async def create_item(gitRepoToInspect: GitRepoToInspect):
     try:
         for ext in extensions:
             if ext == "py":
-                loader = GenericLoader.from_filesystem(
-                    repo_path + "/",
-                    glob="**/*",
-                    suffixes=[".py"],
-                    exclude=["**/non-utf8-encoding.py"],
-                    parser=LanguageParser(language=Language.PYTHON, parser_threshold=500),
-                )
-                documents = loader.load()    
-                python_splitter = RecursiveCharacterTextSplitter.from_language(
-                    language=Language.PYTHON, chunk_size=2000, chunk_overlap=200
-                )
-                texts = python_splitter.split_documents(documents)
-                texts_all_types = texts_all_types + texts
+                texts_all_types += process_files("py", repo_path, Language.PYTHON, True)
             elif ext == "js":
-                loader = GenericLoader.from_filesystem(
-                    repo_path + "/",
-                    glob="**/*",
-                    suffixes=[".js"],
-                    exclude=["**/non-utf8-encoding.js"],
-                    parser=LanguageParser(language=Language.JS, parser_threshold=500),
-                )
-                documents = loader.load()    
-                java_script_splitter = RecursiveCharacterTextSplitter.from_language(
-                    language=Language.JS, chunk_size=2000, chunk_overlap=200
-                )
-                texts = java_script_splitter.split_documents(documents)
-                texts_all_types = texts_all_types + texts
+                texts_all_types += process_files("js", repo_path, Language.JS, True)
             elif ext == "java":
-                loader = GenericLoader.from_filesystem(
-                    repo_path + "/",
-                    glob="**/*",
-                    suffixes=[".java"],
-                    exclude=["**/non-utf8-encoding.java"],
-                    parser=LanguageParser(parser_threshold=500),
-                )
-                documents = loader.load()    
-                java_splitter = RecursiveCharacterTextSplitter.from_language(
-                    language=Language.JAVA, chunk_size=2000, chunk_overlap=200
-                )
-                texts = java_splitter.split_documents(documents)
-                texts_all_types = texts_all_types + texts
+                texts_all_types += process_files("java", repo_path, Language.JAVA, False)
             elif ext == "go":
-                loader = GenericLoader.from_filesystem(
-                    repo_path + "/",
-                    glob="**/*",
-                    suffixes=[".go"],
-                    exclude=["**/non-utf8-encoding.go"],
-                    parser=LanguageParser(parser_threshold=500),
-                )
-                documents = loader.load()    
-                go_splitter = RecursiveCharacterTextSplitter.from_language(
-                    language=Language.GO, chunk_size=2000, chunk_overlap=200
-                )
-                texts = go_splitter.split_documents(documents)
-                texts_all_types = texts_all_types + texts
+                texts_all_types += process_files("go", repo_path, Language.GO, False)
             elif ext == "cs":
-                loader = GenericLoader.from_filesystem(
-                    repo_path + "/",
-                    glob="**/*",
-                    suffixes=[".cs"],
-                    exclude=["**/non-utf8-encoding.cs"],
-                    parser=LanguageParser(parser_threshold=500),
-                )
-                documents = loader.load()    
-                cs_splitter = RecursiveCharacterTextSplitter.from_language(
-                    language=Language.CSHARP, chunk_size=2000, chunk_overlap=200
-                )
-                texts = cs_splitter.split_documents(documents)
-                texts_all_types = texts_all_types + texts
+                texts_all_types += process_files("cs", repo_path, Language.CSHARP, False)
             elif ext == "php":
-                loader = GenericLoader.from_filesystem(
-                    repo_path + "/",
-                    glob="**/*",
-                    suffixes=[".php"],
-                    exclude=["**/non-utf8-encoding.php"],
-                    parser=LanguageParser(parser_threshold=500),
-                )
-                documents = loader.load()    
-                php_splitter = RecursiveCharacterTextSplitter.from_language(
-                    language=Language.PHP, chunk_size=2000, chunk_overlap=200
-                )
-                texts = php_splitter.split_documents(documents)
-                texts_all_types = texts_all_types + texts
+                texts_all_types += process_files("php", repo_path, Language.PHP, False)
             elif ext == "ts":
-                loader = GenericLoader.from_filesystem(
-                    repo_path + "/",
-                    glob="**/*",
-                    suffixes=[".ts"],
-                    exclude=["**/non-utf8-encoding.ts"],
-                    parser=LanguageParser(language=Language.JS, parser_threshold=500),
-                )
-                documents = loader.load()    
-                type_script_splitter = RecursiveCharacterTextSplitter.from_language(
-                    language=Language.TS, chunk_size=2000, chunk_overlap=200
-                )
-                texts = type_script_splitter.split_documents(documents)
-                texts_all_types = texts_all_types + texts
+                texts_all_types += process_files("ts", repo_path, Language.JS, True)
             elif ext == "cpp":
-                loader = GenericLoader.from_filesystem(
-                    repo_path + "/",
-                    glob="**/*",
-                    suffixes=[".cpp"],
-                    exclude=["**/non-utf8-encoding.cpp"],
-                    parser=LanguageParser(language=Language.CPP, parser_threshold=500),
-                )
-                documents = loader.load()    
-                cpp_splitter = RecursiveCharacterTextSplitter.from_language(
-                    language=Language.CPP, chunk_size=2000, chunk_overlap=200
-                )
-                texts = cpp_splitter.split_documents(documents)
-                texts_all_types = texts_all_types + texts
+                texts_all_types += process_files("cpp", repo_path, Language.CPP, True)
             elif ext == "c":
-                loader = GenericLoader.from_filesystem(
-                    repo_path + "/",
-                    glob="**/*",
-                    suffixes=[".c"],
-                    exclude=["**/non-utf8-encoding.c"],
-                    parser=LanguageParser(language=Language.CPP, parser_threshold=500),
-                )
-                documents = loader.load()    
-                c_splitter = RecursiveCharacterTextSplitter.from_language(
-                    language=Language.CPP, chunk_size=2000, chunk_overlap=200
-                )
-                texts = c_splitter.split_documents(documents)
-                texts_all_types = texts_all_types + texts
+                texts_all_types += process_files("c", repo_path, Language.CPP, True)
             elif ext == "swift":
-                loader = GenericLoader.from_filesystem(
-                    repo_path + "/",
-                    glob="**/*",
-                    suffixes=[".swift"],
-                    exclude=["**/non-utf8-encoding.c"],
-                    parser=LanguageParser(parser_threshold=500),
-                )
-                documents = loader.load()    
-                swift_splitter = RecursiveCharacterTextSplitter.from_language(
-                    language=Language.SWIFT, chunk_size=2000, chunk_overlap=200
-                )
-                texts = swift_splitter.split_documents(documents)
-                texts_all_types = texts_all_types + texts
+                texts_all_types += process_files("swift", repo_path, Language.SWIFT, False)
             elif ext == "":
                 loader = GenericLoader.from_filesystem(
                     repo_path + "/",
@@ -234,7 +117,7 @@ async def create_item(gitRepoToInspect: GitRepoToInspect):
                     exclude=["**/non-utf8-encoding.txt"],
                     parser=LanguageParser(parser_threshold=500),
                 )
-                documents = loader.load()    
+                documents = loader.load()
                 text_splitter = RecursiveCharacterTextSplitter.from_language(
                     language=Language.MARKDOWN, chunk_size=2000, chunk_overlap=200
                 )
@@ -252,33 +135,12 @@ async def create_item(gitRepoToInspect: GitRepoToInspect):
 
     return gitRepoToInspect
 
-
 @app.post("/buildOutContainers/")
 async def buildOutContainers(inspectionTask: InspectionTask):
     source_value = texts_global[inspectionTask.iterationNum].metadata['source']
     file_ext = source_value.rsplit('.', 1)[-1]
 
-    code_language = "Python 3"
-    if (file_ext == "js"):
-        code_language = "JavaScript"
-    if (file_ext == "java"):
-        code_language = "Java"
-    if (file_ext == "go"):
-        code_language = "GoLang"
-    if (file_ext == "cs"):
-        code_language = "C#"
-    if (file_ext == "php"):
-        code_language = "PHP"
-    if (file_ext == "ts"):
-        code_language = "TypeScript"
-    if (file_ext == "cpp"):
-        code_language = "C++"
-    if (file_ext == "c"):
-        code_language = "C"
-    if (file_ext == "swift"):
-        code_language = "Swift"
-    if ("Dockerfile" in source_value):
-        code_language = "Docker"
+    code_language = extToLanguage(file_ext)
 
     inspectionTask.metadataSource = texts_global[inspectionTask.iterationNum].metadata
     inspectionTask.messageContent = ""  # will be filled in with streaming request
@@ -286,60 +148,71 @@ async def buildOutContainers(inspectionTask: InspectionTask):
     inspectionTask.codeLanguage = code_language
     return inspectionTask
 
-
 @app.post("/retrieveInspectionsStream/")
 async def processInspectionTaskStream(inspectionTask: InspectionTask):
     source_value = texts_global[inspectionTask.iterationNum].metadata['source']
     file_ext = source_value.rsplit('.', 1)[-1]
 
-    code_language = "Python 3"
-    if (file_ext == "js"):
-        code_language = "JavaScript"
-    if (file_ext == "java"):
-        code_language = "Java"
-    if (file_ext == "go"):
-        code_language = "GoLang"
-    if (file_ext == "cs"):
-        code_language = "C#"
-    if (file_ext == "php"):
-        code_language = "PHP"
-    if (file_ext == "ts"):
-        code_language = "TypeScript"
-    if (file_ext == "cpp"):
-        code_language = "C++"
-    if (file_ext == "c"):
-        code_language = "C"
-    if (file_ext == "swift"):
-        code_language = "Swift"
-    if ("Dockerfile" in source_value):
-        code_language = "Docker"
+    code_language = extToLanguage(file_ext)
 
     client = AsyncOpenAI(
         api_key = os.getenv("API_KEY"),
         base_url = os.getenv("BASE_URL")
     )
 
-    stream = await client.chat.completions.create(
-        messages=[
-        {
-            'role': 'user', 
-            'content': 'Give a short and concise response. Analyze the following ' + code_language + ' code for security vulnerabilities using OWASP guidelines and report any issues found in a brief summary. \n\n ' + texts_global[inspectionTask.iterationNum].page_content
+    try:
+        response_format_string = "text"
+        system_prompt = """"""
+        if os.getenv("JSON_OUTPUT") == "True":
+            response_format_string = "json_object"
+            system_prompt = """
+            Respond with a valid JSON string only, without any additional text or explanations. The output should adhere to the following structure:
+            ```
+            {
+                "security_vulnerabilities": [
+                    {
+                        "vulnerability": "<string>",
+                        "description": "<string>"
+                    }
+                ]
+            }
+            ```
+            Do not include any introductory phrases or sentences in your response. Only provide the JSON object.
+            """
+        stream = await client.chat.completions.create(
+            messages=[
+            {
+                'role': 'system', 
+                'content': system_prompt,
+            },
+            {
+                'role': 'user', 
+                'content': 'Give a short and concise response. Analyze the following ' + code_language + ' code for security vulnerabilities using OWASP guidelines and report any issues found in a brief summary. If there are no serious vulnerabilities found, simply say that none were found. Here is the code: \n\n ' + texts_global[inspectionTask.iterationNum].page_content
+            }
+        ],
+        model = os.getenv("LLM_MODEL"),
+        stream=True,
+        temperature=0.4,
+        response_format={
+            "type": response_format_string
         }
-    ],
-    model = os.getenv("LLM_MODEL"), 
-    stream=True
-    )
-    
-    async def generator():
-        async for chunk in stream:
-            yield chunk.choices[0].delta.content or ""
+        )
+    except Exception as e:
+        print("stream = await Error")
+ 
+    try:
+        async def generator():
+            async for chunk in stream:
+                if chunk.choices:
+                    yield chunk.choices[0].delta.content or ""
+    except Exception as e:
+        print("async def generator Error")
 
     inspectionTask.metadataSource = texts_global[inspectionTask.iterationNum].metadata
     inspectionTask.beginingWith = beginingCodeSnippet(texts_global[inspectionTask.iterationNum].page_content)
     inspectionTask.codeLanguage = code_language
     response_messages = generator()
     return StreamingResponse(response_messages, media_type="text/event-stream")
-
 
 @app.post("/retrieveRemediationsStream/")
 async def processRemediationTask(remediateTask: RemediateTask):
@@ -365,33 +238,66 @@ async def processRemediationTask(remediateTask: RemediateTask):
 
     async def generator():
         async for chunk in stream:
-            yield chunk.choices[0].delta.content or ""
+            if chunk.choices:
+                yield chunk.choices[0].delta.content or ""
     
     response_messages = generator()
     return StreamingResponse(response_messages, media_type="text/event-stream")
 
 
+def process_files(ext, repo_path, language, use_parser_language):
+    if use_parser_language == True:
+        loader = GenericLoader.from_filesystem(
+            repo_path + "/",
+            glob="**/*",
+            suffixes=[f".{ext}"],
+            exclude=[f"**/non-utf8-encoding.{ext}"],
+            parser=LanguageParser(language=language, parser_threshold=500),
+        )
+    if use_parser_language == False:
+        loader = GenericLoader.from_filesystem(
+            repo_path + "/",
+            glob="**/*",
+            suffixes=[f".{ext}"],
+            exclude=[f"**/non-utf8-encoding.{ext}"],
+            parser=LanguageParser(parser_threshold=500),
+        )
+    documents = loader.load()
+    splitter = RecursiveCharacterTextSplitter.from_language(
+        language=language, chunk_size=2000, chunk_overlap=200
+    )
+    return splitter.split_documents(documents)
+
+
 def beginingCodeSnippet(page_content):
     firstFiftyChars = str(page_content)[:50]
     listSplitOnCarriageReturn = firstFiftyChars.splitlines(True)
-    # firstLine = listSplitOnCarriageReturn[0]
     return listSplitOnCarriageReturn[0]
 
 
-def increase_number_suffix(string):
-    # Regular expression to match underscore followed by a number at the end of the string
-    pattern = r'_(\d+)$'
-    match = re.search(pattern, string)
-    if match:
-        # Extract the number and increment it
-        number = int(match.group(1))
-        new_number = number + 1
-        # Replace the old number with the new one in the string
-        return re.sub(pattern, '_' + str(new_number), string)
-    else:
-        return string
-
-
-
-
-
+def extToLanguage(file_ext):
+    code_language = "Python 3"
+    if (file_ext == "js"):
+        code_language = "JavaScript"
+    if (file_ext == "java"):
+        code_language = "Java"
+    if (file_ext == "go"):
+        code_language = "GoLang"
+    if (file_ext == "cs"):
+        code_language = "C#"
+    if (file_ext == "php"):
+        code_language = "PHP"
+    if (file_ext == "ts"):
+        code_language = "TypeScript"
+    if (file_ext == "cpp"):
+        code_language = "C++"
+    if (file_ext == "c"):
+        code_language = "C"
+    if (file_ext == "swift"):
+        code_language = "Swift"
+    if (file_ext == "lua"):
+        code_language = "Lua"
+    if (file_ext == "Dockerfile"):
+        code_language = "Docker"
+    return code_language
+    
